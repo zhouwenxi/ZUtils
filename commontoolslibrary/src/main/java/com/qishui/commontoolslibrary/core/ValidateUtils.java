88f6d8886858db9205c1d26d08ad152e9aa52985
zhouwenxi <qishuichixi@126.com>
2018-12-24 15:22:09 +0800

    添加验证文件工具

---
+++ commontoolslibrary/src/main/java/com/qishui/commontoolslibrary/core/ValidateUtils.java
@@ -0,0 +1,14 @@
+package com.qishui.commontoolslibrary.core;
+
+/**
+ * author： qishui
+ * date: 2018/12/24  10:55
+ * email: qihsuichixi@163.com
+ * qq: 798150439
+ * blog: http://zhouwenxi.top
+ * desc: 验证
+ */
+public class ValidateUtils {
+
+
+}
