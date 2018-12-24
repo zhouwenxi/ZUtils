8316c48ddad7c9b34c0300a2fa8b74c60f7285e0
zhouwenxi <qishuichixi@126.com>
2018-12-24 17:26:04 +0800

    同步代码

--- commontoolslibrary/src/main/java/com/qishui/commontoolslibrary/core/PermissionUtils.java
+++ commontoolslibrary/src/main/java/com/qishui/commontoolslibrary/core/PermissionUtils.java
@@ -1,8 +1,456 @@
 package com.qishui.commontoolslibrary.core;
 
+import android.Manifest;
+import android.Manifest.permission;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.annotation.StringDef;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.WindowManager;
+
+import com.qishui.commontoolslibrary.base.BaseQiShuiApplication;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Created by zhou on 2018/12/23.
  */
 
 public class PermissionUtils {
+
+
+    private static final List<String> PERMISSIONS = getPermissions();
+
+    private static PermissionUtils sInstance;
+
+    private OnRationaleListener mOnRationaleListener;
+    private SimpleCallback mSimpleCallback;
+    private FullCallback mFullCallback;
+    private ThemeCallback mThemeCallback;
+    private Set<String> mPermissions;
+    private List<String> mPermissionsRequest;
+    private List<String> mPermissionsGranted;
+    private List<String> mPermissionsDenied;
+    private List<String> mPermissionsDeniedForever;
+
+    /**
+     * Return the permissions used in application.
+     *
+     * @return the permissions used in application
+     */
+    public static List<String> getPermissions() {
+        return getPermissions(BaseQiShuiApplication.getContext().getPackageName());
+    }
+
+    /**
+     * Return the permissions used in application.
+     *
+     * @param packageName The name of the package.
+     * @return the permissions used in application
+     */
+    public static List<String> getPermissions(final String packageName) {
+        PackageManager pm = BaseQiShuiApplication.getContext().getPackageManager();
+        try {
+            return Arrays.asList(
+                    pm.getPackageInfo(packageName, PackageManager.GET_PERMISSIONS)
+                            .requestedPermissions
+            );
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
+    }
+
+    /**
+     * Return whether <em>you</em> have granted the permissions.
+     *
+     * @param permissions The permissions.
+     * @return {@code true}: yes<br>{@code false}: no
+     */
+    public static boolean isGranted(final String... permissions) {
+        for (String permission : permissions) {
+            if (!isGranted(permission)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isGranted(final String permission) {
+        return Build.VERSION.SDK_INT < Build.VERSION_CODES.M
+                || PackageManager.PERMISSION_GRANTED
+                == ContextCompat.checkSelfPermission(BaseQiShuiApplication.getContext(), permission);
+    }
+
+    /**
+     * Launch the application's details settings.
+     */
+    public static void launchAppDetailsSettings() {
+        Intent intent = new Intent("android.settings.APPLICATION_DETAILS_SETTINGS");
+        intent.setData(Uri.parse("package:" + BaseQiShuiApplication.getContext().getPackageName()));
+        BaseQiShuiApplication.getContext().startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+    }
+
+    /**
+     * Set the permissions.
+     *
+     * @param permissions The permissions.
+     * @return the single {@link PermissionUtils} instance
+     */
+    public static PermissionUtils permission(final String... permissions) {
+        return new PermissionUtils(permissions);
+    }
+
+    private PermissionUtils(final String... permissions) {
+        mPermissions = new LinkedHashSet<>();
+        for (String permission : permissions) {
+            for (String aPermission : getPermissionsGroup(permission)) {
+                if (PERMISSIONS.contains(aPermission)) {
+                    mPermissions.add(aPermission);
+                }
+            }
+        }
+        sInstance = this;
+    }
+
+    /**
+     * Set rationale listener.
+     *
+     * @param listener The rationale listener.
+     * @return the single {@link PermissionUtils} instance
+     */
+    public PermissionUtils rationale(final OnRationaleListener listener) {
+        mOnRationaleListener = listener;
+        return this;
+    }
+
+    /**
+     * Set the simple call back.
+     *
+     * @param callback the simple call back
+     * @return the single {@link PermissionUtils} instance
+     */
+    public PermissionUtils callback(final SimpleCallback callback) {
+        mSimpleCallback = callback;
+        return this;
+    }
+
+    /**
+     * Set the full call back.
+     *
+     * @param callback the full call back
+     * @return the single {@link PermissionUtils} instance
+     */
+    public PermissionUtils callback(final FullCallback callback) {
+        mFullCallback = callback;
+        return this;
+    }
+
+    /**
+     * Set the theme callback.
+     *
+     * @param callback The theme callback.
+     * @return the single {@link PermissionUtils} instance
+     */
+    public PermissionUtils theme(final ThemeCallback callback) {
+        mThemeCallback = callback;
+        return this;
+    }
+
+    /**
+     * Start request.
+     */
+    public void request() {
+        mPermissionsGranted = new ArrayList<>();
+        mPermissionsRequest = new ArrayList<>();
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            mPermissionsGranted.addAll(mPermissions);
+            requestCallback();
+        } else {
+            for (String permission : mPermissions) {
+                if (isGranted(permission)) {
+                    mPermissionsGranted.add(permission);
+                } else {
+                    mPermissionsRequest.add(permission);
+                }
+            }
+            if (mPermissionsRequest.isEmpty()) {
+                requestCallback();
+            } else {
+                startPermissionActivity();
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void startPermissionActivity() {
+        mPermissionsDenied = new ArrayList<>();
+        mPermissionsDeniedForever = new ArrayList<>();
+        PermissionActivity.start(BaseQiShuiApplication.getContext());
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private boolean rationale(final Activity activity) {
+        boolean isRationale = false;
+        if (mOnRationaleListener != null) {
+            for (String permission : mPermissionsRequest) {
+                if (activity.shouldShowRequestPermissionRationale(permission)) {
+                    getPermissionsStatus(activity);
+                    mOnRationaleListener.rationale(new OnRationaleListener.ShouldRequest() {
+                        @Override
+                        public void again(boolean again) {
+                            if (again) {
+                                startPermissionActivity();
+                            } else {
+                                requestCallback();
+                            }
+                        }
+                    });
+                    isRationale = true;
+                    break;
+                }
+            }
+            mOnRationaleListener = null;
+        }
+        return isRationale;
+    }
+
+    private void getPermissionsStatus(final Activity activity) {
+        for (String permission : mPermissionsRequest) {
+            if (isGranted(permission)) {
+                mPermissionsGranted.add(permission);
+            } else {
+                mPermissionsDenied.add(permission);
+                if (!activity.shouldShowRequestPermissionRationale(permission)) {
+                    mPermissionsDeniedForever.add(permission);
+                }
+            }
+        }
+    }
+
+    private void requestCallback() {
+        if (mSimpleCallback != null) {
+            if (mPermissionsRequest.size() == 0
+                    || mPermissions.size() == mPermissionsGranted.size()) {
+                mSimpleCallback.onGranted();
+            } else {
+                if (!mPermissionsDenied.isEmpty()) {
+                    mSimpleCallback.onDenied();
+                }
+            }
+            mSimpleCallback = null;
+        }
+        if (mFullCallback != null) {
+            if (mPermissionsRequest.size() == 0
+                    || mPermissions.size() == mPermissionsGranted.size()) {
+                mFullCallback.onGranted(mPermissionsGranted);
+            } else {
+                if (!mPermissionsDenied.isEmpty()) {
+                    mFullCallback.onDenied(mPermissionsDeniedForever, mPermissionsDenied);
+                }
+            }
+            mFullCallback = null;
+        }
+        mOnRationaleListener = null;
+        mThemeCallback = null;
+    }
+
+    private void onRequestPermissionsResult(final Activity activity) {
+        getPermissionsStatus(activity);
+        requestCallback();
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public static class PermissionActivity extends Activity {
+
+        public static void start(final Context context) {
+            Intent starter = new Intent(context, PermissionActivity.class);
+            starter.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(starter);
+        }
+
+        @Override
+        protected void onCreate(@Nullable Bundle savedInstanceState) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+                    | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH);
+            if (sInstance == null) {
+                super.onCreate(savedInstanceState);
+                Log.e("PermissionUtils", "request permissions failed");
+                finish();
+                return;
+            }
+            if (sInstance.mThemeCallback != null) {
+                sInstance.mThemeCallback.onActivityCreate(this);
+            }
+            super.onCreate(savedInstanceState);
+
+            if (sInstance.rationale(this)) {
+                finish();
+                return;
+            }
+            if (sInstance.mPermissionsRequest != null) {
+                int size = sInstance.mPermissionsRequest.size();
+                if (size <= 0) {
+                    finish();
+                    return;
+                }
+                requestPermissions(sInstance.mPermissionsRequest.toArray(new String[size]), 1);
+            }
+        }
+
+        @Override
+        public void onRequestPermissionsResult(int requestCode,
+                                               @NonNull String[] permissions,
+                                               @NonNull int[] grantResults) {
+            sInstance.onRequestPermissionsResult(this);
+            finish();
+        }
+
+        @Override
+        public boolean dispatchTouchEvent(MotionEvent ev) {
+            finish();
+            return true;
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // interface
+    ///////////////////////////////////////////////////////////////////////////
+
+    public interface OnRationaleListener {
+
+        void rationale(ShouldRequest shouldRequest);
+
+        interface ShouldRequest {
+            void again(boolean again);
+        }
+    }
+
+    public interface SimpleCallback {
+        void onGranted();
+
+        void onDenied();
+    }
+
+    public interface FullCallback {
+        void onGranted(List<String> permissionsGranted);
+
+        void onDenied(List<String> permissionsDeniedForever, List<String> permissionsDenied);
+    }
+
+    public interface ThemeCallback {
+        void onActivityCreate(Activity activity);
+    }
+
+    ////////////////////////////////////////////////////////////////////////
+    //权限分组管理
+    ///////////////////////////////////////////////////////////////////////
+    /**
+     * 日历
+     */
+    public static final String CALENDAR = Manifest.permission_group.CALENDAR;
+    /**
+     * 相机
+     */
+    public static final String CAMERA = Manifest.permission_group.CAMERA;
+    public static final String CONTACTS = Manifest.permission_group.CONTACTS;
+    public static final String LOCATION = Manifest.permission_group.LOCATION;
+    public static final String MICROPHONE = Manifest.permission_group.MICROPHONE;
+    public static final String PHONE = Manifest.permission_group.PHONE;
+    public static final String SENSORS = Manifest.permission_group.SENSORS;
+    public static final String SMS = Manifest.permission_group.SMS;
+    public static final String STORAGE = Manifest.permission_group.STORAGE;
+
+    private static final String[] GROUP_CALENDAR = {
+            permission.READ_CALENDAR, permission.WRITE_CALENDAR
+    };
+    private static final String[] GROUP_CAMERA = {
+            permission.CAMERA
+    };
+    private static final String[] GROUP_CONTACTS = {
+            permission.READ_CONTACTS, permission.WRITE_CONTACTS, permission.GET_ACCOUNTS
+    };
+    private static final String[] GROUP_LOCATION = {
+            permission.ACCESS_FINE_LOCATION, permission.ACCESS_COARSE_LOCATION
+    };
+    private static final String[] GROUP_MICROPHONE = {
+            permission.RECORD_AUDIO
+    };
+    private static final String[] GROUP_PHONE = {
+            permission.READ_PHONE_STATE, permission.READ_PHONE_NUMBERS, permission.CALL_PHONE,
+            permission.READ_CALL_LOG, permission.WRITE_CALL_LOG, permission.ADD_VOICEMAIL,
+            permission.USE_SIP, permission.PROCESS_OUTGOING_CALLS, permission.ANSWER_PHONE_CALLS
+    };
+    private static final String[] GROUP_PHONE_BELOW_O = Arrays.copyOf(
+            GROUP_PHONE, GROUP_PHONE.length - 1
+    );
+    private static final String[] GROUP_SENSORS = {
+            permission.BODY_SENSORS
+    };
+    private static final String[] GROUP_SMS = {
+            permission.SEND_SMS, permission.RECEIVE_SMS, permission.READ_SMS,
+            permission.RECEIVE_WAP_PUSH, permission.RECEIVE_MMS,
+    };
+    private static final String[] GROUP_STORAGE = {
+            permission.READ_EXTERNAL_STORAGE, permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    @StringDef({CALENDAR, CAMERA, CONTACTS, LOCATION, MICROPHONE, PHONE, SENSORS, SMS, STORAGE,})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Permission {
+    }
+
+    /**
+     * 获取权限组
+     *
+     * @param permission
+     * @return
+     */
+    public static String[] getPermissionsGroup(final String permission) {
+        switch (permission) {
+            case CALENDAR:
+                return GROUP_CALENDAR;
+            case CAMERA:
+                return GROUP_CAMERA;
+            case CONTACTS:
+                return GROUP_CONTACTS;
+            case LOCATION:
+                return GROUP_LOCATION;
+            case MICROPHONE:
+                return GROUP_MICROPHONE;
+            case PHONE:
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+                    return GROUP_PHONE_BELOW_O;
+                } else {
+                    return GROUP_PHONE;
+                }
+            case SENSORS:
+                return GROUP_SENSORS;
+            case SMS:
+                return GROUP_SMS;
+            case STORAGE:
+                return GROUP_STORAGE;
+        }
+        return new String[]{permission};
+    }
+
+
 }
