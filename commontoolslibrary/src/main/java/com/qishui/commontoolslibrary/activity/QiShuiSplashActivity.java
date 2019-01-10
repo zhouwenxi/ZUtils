3c3c3fb22ce7136d7c32dc08bd23ef380efb065b
zhouwenxi <qishuichixi@126.com>
2019-01-10 14:39:38 +0800

    完善启动页

--- commontoolslibrary/src/main/java/com/qishui/commontoolslibrary/activity/QiShuiSplashActivity.java
+++ commontoolslibrary/src/main/java/com/qishui/commontoolslibrary/activity/QiShuiSplashActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.TextUtils;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
@@ -42,9 +43,12 @@ public abstract class QiShuiSplashActivity extends BaseQiShuiActivity {
      */
     private TextView qishui_splash_tv;
 
+    private TextView qishui_splash_tv_copyright;
+
     private int color = 0;
     private int icon = 0;
-    private String msg;
+    private String versionMsg;
+    private String copyrightMsg;
     private int time = 3;
 
 
@@ -53,14 +57,13 @@ public abstract class QiShuiSplashActivity extends BaseQiShuiActivity {
         //设置全屏
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         return R.layout.activity_qi_shui_splash;
     }
 
     @Override
     protected void initEvent(Bundle savedInstanceState) {
         bindView();
-        setData(color, icon, msg,time);
+        setData(color, icon, versionMsg,copyrightMsg,time);
         toNext(setActivity());
     }
 
@@ -75,8 +78,46 @@ public abstract class QiShuiSplashActivity extends BaseQiShuiActivity {
         qishui_splash_rl = findViewById(R.id.qishui_splash_rl);
         qishui_splash_iv = findViewById(R.id.qishui_splash_iv);
         qishui_splash_tv = findViewById(R.id.qishui_splash_tv);
+        qishui_splash_tv_copyright=findViewById(R.id.qishui_splash_tv_copyright);
     }
 
+
+    /**
+     * 设置是否需要图标
+     * @param flag
+     */
+    public void setIconVisibility(Boolean flag) {
+        if(flag){
+            this.qishui_splash_iv.setVisibility(View.VISIBLE);
+        }else {
+            this.qishui_splash_iv.setVisibility(View.GONE);
+        }
+    }
+
+    /**
+     * 设置版本信息
+     * @param flag
+     */
+    public void setVersionVisibility(Boolean flag) {
+        if(flag){
+            this.qishui_splash_tv.setVisibility(View.VISIBLE);
+        }else {
+            this.qishui_splash_tv.setVisibility(View.GONE);
+        }
+    }
+
+    /**
+     * 设置版权信息
+     * @param flag
+     */
+    public void setCopyrightVisibility(Boolean flag) {
+
+        if(flag){
+            this.qishui_splash_tv_copyright.setVisibility(View.VISIBLE);
+        }else {
+            this.qishui_splash_tv_copyright.setVisibility(View.GONE);
+        }
+    }
 
     /**
      * 设置数据
@@ -85,10 +126,12 @@ public abstract class QiShuiSplashActivity extends BaseQiShuiActivity {
      * @param icon
      * @param msg
      */
-    public void setData(int color, int icon, String msg,int time) {
+    public void setData(int color, int icon, String msg,String copyrightMsg,int time) {
+
         this.color = color;
         this.icon = icon;
-        this.msg = msg;
+        this.versionMsg = msg;
+        this.copyrightMsg=copyrightMsg;
         this.time=time;
 
         if (color != 0) {
@@ -98,10 +141,14 @@ public abstract class QiShuiSplashActivity extends BaseQiShuiActivity {
             qishui_splash_iv.setImageResource(icon);
         }
 
+        if (TextUtils.isEmpty(copyrightMsg)) {
+            copyrightMsg="copyright@ 2018 by QiShui";
+        }
+        qishui_splash_tv_copyright.setText(copyrightMsg);
+
         if (TextUtils.isEmpty(msg)) {
             msg="当前版本: "+AppUtils.getAppVirsionName();
         }
-
         qishui_splash_tv.setText(msg);
 
         //设置最大时间5s
